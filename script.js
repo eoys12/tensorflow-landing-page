5ba85456dcdecd7bba7f1c8363ed37e8f82273d2
Edward Ong <58973929+eoys12@users.noreply.github.com>
2023-03-10 13:28:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,46 @@ async function loadMobileNetFeatureModel() {
   
 // Call the function immediately to start loading.
 loadMobileNetFeatureModel();
+
+let model = tf.sequential();
+model.add(tf.layers.dense({inputShape: [1024], units: 128, activation: 'relu'}));
+model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
+
+model.summary();
+
+// Compile the model with the defined optimizer and specify a loss function to use.
+model.compile({
+  // Adam changes the learning rate over time which is useful.
+  optimizer: 'adam',
+  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  // Else categoricalCrossentropy is used if more than 2 classes.
+  loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy', 
+  // As this is a classification problem you can record accuracy in the logs too!
+  metrics: ['accuracy']  
+});
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+function enableCam() {
+  if (hasGetUserMedia()) {
+    // getUsermedia parameters.
+    const constraints = {
+      video: true,
+      width: 640, 
+      height: 480 
+    };
+
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      VIDEO.srcObject = stream;
+      VIDEO.addEventListener('loadeddata', function() {
+        videoPlaying = true;
+        ENABLE_CAM_BUTTON.classList.add('removed');
+      });
+    });
+  } else {
+    console.warn('getUserMedia() is not supported by your browser');
+  }
+}
